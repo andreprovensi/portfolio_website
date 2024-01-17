032b2391f78b289f10475a36b5f8fb27defd8c98
andreprovensi <andreprovensi@gmail.com>
2024-01-17 20:47:00 -0300

    added my profile photo

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -38,19 +38,22 @@ const HeroSection = () => {
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit.Quisam, volupatum.
                 </p>
                 <div>
-                  <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>Hire Me</button>
-                  {/* <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-neutral-900 via-neutral-600 to-slate-50 hover:bg-slate-200 text-white'>Hire Me</button> */}
+                  {/* <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>Hire Me</button> */}
+                  <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-neutral-900 via-neutral-600 to-slate-50 hover:bg-slate-200 text-white border border-white'>Hire Me</button>
+
                   <button className='px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500  via-purple-500 to-pink-500 hover:bg-slate-800 text-white border mt-3'>
                     <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
                   </button>
+
                 </div>
             </div>
             <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
               <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                 <Image
-                  src="/images/hero-image.png"
+                  // src="/images/hero-image.png"
+                  src="/images/andre_profile.png"
                   alt="hero image"
-                  className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+                  className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full'
                   width={300}
                   height={300}
                 />
