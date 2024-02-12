177c531f5934d0205c09c1bfa3bb90d7f17f0e98
andreprovensi <andreprovensi@gmail.com>
2024-02-12 19:11:59 -0300

    to create achievements section

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+"use client"
+import React, {useState} from 'react'
 import GithubIcon from '../../../public/github-icon.svg';
 import LinkedinIcon from '../../../public/linkedin-icon.svg';
 import Link from 'next/link';
 import Image from 'next/image';
 
 const EmailSection = () => {
-  return (
+    const [emailSubmitted, setEmailSubmitted] = useState(false);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            email: e.target.email.value,
+            subject: e.target.subject.value,
+            message: e.target.message.value
+        }
+
+        const JSONdata = JSON.stringify(data);
+        const endpoint = "/api/send";
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body:JSONdata
+        }
+
+        const response = await fetch(endpoint, options);
+        const resData = await response.json();
+        console.log(resData);
+
+        if (response.status == 200){
+            console.log("Message sent.");
+        }
+    }
+
+    return (
     <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'>
         <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-500 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2'>
         </div>
@@ -30,14 +60,14 @@ const EmailSection = () => {
         </div>
 
         <div>
-            <form className='flex flex-col'>
+            <form className='flex flex-col' onSubmit={handleSubmit}>
                 <div className='mb-6'>
                     <label htmlFor="email" className='text-white block mb-2 text-sm font-medium'>Your email</label>
-                    <input type="email" id='email' required className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' placeholder='youremail@gmail.com' />
+                    <input name='email' type="email" id='email' required className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' placeholder='youremail@gmail.com' />
                 </div>
                 <div className='mb-6'>
                     <label htmlFor="subject" className='text-white block mb-2 text-sm font-medium'>Subject</label>
-                    <input type="text" id='subject' required className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' placeholder='Your email content' />
+                    <input name='subject' type="text" id='subject' required className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5' placeholder='Your email content' />
                 </div>
                 <div className='mb-6'>
                     <label htmlFor="message" className='text-white block mb-2 text-sm font-medium'>Message</label>
