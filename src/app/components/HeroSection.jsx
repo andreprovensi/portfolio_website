13e68c0dbafd2b52e43206d81209986cdd4d2c8a
andreprovensi <andreprovensi@gmail.com>
2024-02-12 19:51:58 -0300

    finished email section and set primary and secondary colors

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,11 +9,11 @@ import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
   return (
-    <section>
+    <section className='lg:py-16'>
         <div className='grid grid-cols-1 sm:grid-cols-12'>
-            <div className='col-span-7 place-self-center text-center sm:text-left'>
-                <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-                  <span className="text-transparent bg-clip-text bg-gradient-to-r from bg-blue-400 to-slate-50">
+            <div className='col-span-8 place-self-center text-center sm:text-left justify-self-start'>
+                <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold'>
+                  <span className="text-transparent bg-clip-text bg-gradient-to-r from bg-primary-400 to-secondary-50">
                     Hello, I'm{" "}
                   </span>
                   <br /> 
@@ -46,7 +46,7 @@ const HeroSection = () => {
 
                 </div>
             </div>
-            <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+            <div className='col-span-4 place-self-center mt-4 lg:mt-0'>
               <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                 <Image
                   // src="/images/hero-image.png"
--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const TabButton = ({active, selectTab, children}) => {
-    const buttonClasses = active ? "text-white border-b border-purple-500" : "text-[#ADB7BE] border-purple-500"
+    const buttonClasses = active ? "text-white border-b border-primary-500" : "text-[#ADB7BE] border-primary-500"
   return (
     <button onClick={selectTab}>
         <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
