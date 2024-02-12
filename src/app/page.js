ddfce9089e58cde5a8a2c63bfbdc812e5c389c5e
andreprovensi <andreprovensi@gmail.com>
2024-02-12 15:25:21 -0300

    added footer

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import AboutSection from './components/AboutSection';
 import ProjectsSection from './components/ProjectsSection';
 import EmailSection from './components/EmailSection';
-
+import Footer from './components/Footer';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
@@ -15,6 +15,7 @@ export default function Home() {
         <ProjectsSection />
         <EmailSection/>
       </div>
+        <Footer />
     </main>
   )
 }
